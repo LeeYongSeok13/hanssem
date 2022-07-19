3ea5f0421a9e740134a0696278036529c78d321c
LeeYongSeok13 <minamiharuka333@gmail.com>
2022-07-19 17:50:45 +0900

    20220719

--- js/main.js
+++ js/main.js
@@ -3,4 +3,20 @@ $(function () {
         arrows: false,
         dots: true,
     });
+
+    $('.angle i:nth-child(1)').on('click', function () {
+        $('.main_slider').slick('slickPrev');
+    });
+    $('.angle i:nth-child(2)').on('click', function () {
+        $('.main_slider').slick('slickNext');
+    });
+
+    $('.tab_menu li').on('click', function (event) {
+        event.preventDefault();
+        var idx = $(this).index();
+        $('.tab_inner>div').removeClass('on');
+        $('.tab_inner>div').eq(idx).addClass('on');
+        $('.tab_menu li').removeClass('on');
+        $('.tab_menu li').eq(idx).addClass('on');
+    })
 })
