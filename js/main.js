7d617a90247e2da73692e87a53e24b5cd9a2a77a
LeeYongSeok13 <minamiharuka333@gmail.com>
2022-07-20 17:48:26 +0900

    20220720

--- js/main.js
+++ js/main.js
@@ -11,12 +11,24 @@ $(function () {
         $('.main_slider').slick('slickNext');
     });
 
-    $('.tab_menu li').on('click', function (event) {
+    $('.package_slider').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+    });
+
+    $('.package_arrow:nth-child(1)').on('click', function () {
+        $('.package_slider').slick('slickPrev');
+    });
+    $('.package_arrow:nth-child(2)').on('click', function () {
+        $('.package_slider').slick('slickNext');
+    });
+    $('.tab_menu dt').on('click', function (event) {
         event.preventDefault();
         var idx = $(this).index();
         $('.tab_inner>div').removeClass('on');
         $('.tab_inner>div').eq(idx).addClass('on');
-        $('.tab_menu li').removeClass('on');
-        $('.tab_menu li').eq(idx).addClass('on');
+        $('.tab_menu dt').removeClass('on');
+        $('.tab_menu dt').eq(idx).addClass('on');
     })
 })
